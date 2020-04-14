f4a8fabcf29fee9a78dfb7f5c767a51d403c1d44
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2020-04-14 17:05:45 -0400

    save me

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -98,7 +98,6 @@ struct gadget_info {
 	struct work_struct work;
 	struct device *dev;
 #endif
-	bool secure;
 };
 
 static inline struct gadget_info *to_gadget_info(struct config_item *item)
@@ -288,19 +287,11 @@ static int unregister_gadget(struct gadget_info *gi)
 	if (!gi->composite.gadget_driver.udc_name)
 		return -ENODEV;
 
-	if (gi->secure) {
-		gi->unbinding = false;
-		kfree(gi->composite.gadget_driver.udc_name);
-		gi->composite.gadget_driver.udc_name = NULL;
-		return 0;
-	}
-
 	gi->unbinding = true;
 	ret = usb_gadget_unregister_driver(&gi->composite.gadget_driver);
-	if (ret) {
-		pr_err("unregister gadget failed, ret = %d\n", ret);
+	if (ret)
 		return ret;
-	}
+
 	gi->unbinding = false;
 	kfree(gi->composite.gadget_driver.udc_name);
 	gi->composite.gadget_driver.udc_name = NULL;
@@ -320,8 +311,6 @@ static ssize_t gadget_dev_desc_UDC_store(struct config_item *item,
 	if (name[len - 1] == '\n')
 		name[len - 1] = '\0';
 
-	pr_debug("UDC store = %s\n", name);
-
 	mutex_lock(&gi->lock);
 
 	if (!strlen(name) || strcmp(name, "none") == 0) {
@@ -335,12 +324,10 @@ static ssize_t gadget_dev_desc_UDC_store(struct config_item *item,
 			goto err;
 		}
 		gi->composite.gadget_driver.udc_name = name;
-		if (!gi->secure) {
-			ret = usb_gadget_probe_driver(&gi->composite.gadget_driver);
-			if (ret) {
-				gi->composite.gadget_driver.udc_name = NULL;
-				goto err;
-			}
+		ret = usb_gadget_probe_driver(&gi->composite.gadget_driver);
+		if (ret) {
+			gi->composite.gadget_driver.udc_name = NULL;
+			goto err;
 		}
 	}
 	mutex_unlock(&gi->lock);
@@ -1361,8 +1348,7 @@ static int configfs_composite_bind(struct usb_gadget *gadget,
 		cdev->use_os_string = true;
 		cdev->b_vendor_code = gi->b_vendor_code;
 		memcpy(cdev->qw_sign, gi->qw_sign, OS_STRING_QW_SIGN_LEN);
-	} else
-		cdev->use_os_string = false;
+	}
 
 	if (gadget_is_otg(gadget) && !otg_desc[0]) {
 		struct usb_descriptor_header *usb_desc;
@@ -1649,69 +1635,8 @@ out:
 
 static DEVICE_ATTR(state, S_IRUGO, state_show, NULL);
 
-static ssize_t secure_show(struct device *pdev, struct device_attribute *attr,
-			char *buf)
-{
-	struct gadget_info *gi = dev_get_drvdata(pdev);
-
-	if (!gi)
-		return -ENODEV;
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", gi->secure);
-}
-
-static ssize_t secure_store(struct device *pdev, struct device_attribute *attr,
-			const char *buf, size_t count)
-{
-	struct gadget_info *gi = dev_get_drvdata(pdev);
-	unsigned long mode, r;
-	int ret;
-
-	if (!gi)
-		return -ENODEV;
-
-	r = kstrtoul(buf, 0, &mode);
-	if (r) {
-		dev_err(pdev, "Invalid value = %lu\n", mode);
-		return -EINVAL;
-	}
-
-	mode = !!mode;
-	if (mode == gi->secure)
-		return count;
-
-	mutex_lock(&gi->lock);
-
-	gi->secure = mode;
-
-	if (!gi->composite.gadget_driver.udc_name) {
-		mutex_unlock(&gi->lock);
-		return count;
-	}
-	pr_debug("Secure Store , UDC = %s, secure = %d\n",
-				gi->composite.gadget_driver.udc_name,
-				gi->secure);
-
-	if (gi->secure) {
-		ret = usb_gadget_unregister_driver(
-				&gi->composite.gadget_driver);
-		if (ret)
-			pr_err("Failed detaching UDC from gadget %d\n", ret);
-	} else {
-		ret = usb_gadget_probe_driver(&gi->composite.gadget_driver);
-		if (ret)
-			pr_err("Failed attaching UDC to gadget %d\n", ret);
-	}
-	mutex_unlock(&gi->lock);
-
-	return count;
-}
-
-static DEVICE_ATTR(secure, S_IRUGO | S_IWUSR, secure_show, secure_store);
-
 static struct device_attribute *android_usb_attributes[] = {
 	&dev_attr_state,
-	&dev_attr_secure,
 	NULL
 };
 
@@ -1803,7 +1728,6 @@ static struct config_group *gadgets_make(
 	gi->composite.resume = NULL;
 	gi->composite.max_speed = USB_SPEED_SUPER;
 
-	spin_lock_init(&gi->spinlock);
 	mutex_init(&gi->lock);
 	INIT_LIST_HEAD(&gi->string_list);
 	INIT_LIST_HEAD(&gi->available_func);
